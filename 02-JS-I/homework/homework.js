ba9ee48e5b2c2fdb8e9566515ba86a66842a1466
Alejandro <alejourrego0129@gmail.com>
2022-01-08 01:43:45 -0500

    hw 2 finished

--- 02-JS-I/homework/homework.js
+++ 02-JS-I/homework/homework.js
@@ -180,14 +180,12 @@ function esPositivo(numero) {
   //Si el número es positivo, devolver ---> "Es positivo"
   //Si el número es negativo, devolver ---> "Es negativo"
   //Si el número es 0, devuelve false
-  if(numero === 0) {
+  if(numero > 0) {
+    return "Es positivo"
+  } else if(numero < 0) {
+    return "Es negativo" 
+  } else {
     return false;
-  }
-  else if(numero > 0) {
-    return "Es positivo";
-  }
-  else {
-    return "Es negativo";
   }
 }
 
@@ -212,7 +210,7 @@ function obtenerSaludo(nombre) {
   // Tu código:
   return 'Hola ${nombre}!'
 }
-c
+
 function obtenerAreaRectangulo(alto, ancho) {
   // Retornar el area de un rectángulo teniendo su altura y ancho
   // Tu código:
